4441141298b992f84329f7babbb4dc4183f452f5
Nicolas Humbert <nicolas.humbert@live.fr>
2016-11-22 16:52:12 -0800

    [squash me] add log level env variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,23 @@
 set -e
 
 if [[ "$ACCESS_KEY" && "$SECRET_KEY" ]]; then
-  sed -i "s/accessKey1/$ACCESS_KEY/" ./conf/authdata.json
-  sed -i "s/verySecretKey1/$SECRET_KEY/" ./conf/authdata.json
-  echo "access key and secret key have been modified successfuly"
+    sed -i "s/accessKeyDocker/$ACCESS_KEY/" ./conf/authdata.json
+    sed -i "s/verySecretKeyDocker/$SECRET_KEY/" ./conf/authdata.json
+    echo "Access key and secret key have been modified successfuly"
 fi
 
 if [[ "$HOST_NAME" ]]; then
-  sed -i "s/s3.docker.test/$HOST_NAME/" ./config.json
-  echo "host name has been modified successfuly"
+    sed -i "s/s3.docker.test/$HOST_NAME/" ./config.json
+    echo "Host name has been modified to $HOST_NAME"
+fi
+
+if [[ "$LOG_LEVEL" ]]; then
+    if [[ "$LOG_LEVEL" == "info" || "$LOG_LEVEL" == "debug" || "$LOG_LEVEL" == "trace" ]]; then
+        sed -i "s/\"logLevel\": \"info\"/\"logLevel\": \"$LOG_LEVEL\"/" ./config.json
+        echo "Log level has been modified to $LOG_LEVEL"
+    else
+        echo "The log level you provided is incorrect (info/debug/trace)"
+    fi
 fi
 
 exec "$@"
